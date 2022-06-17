b46167fe06005a02ce71b63990db7c30655cb3c3
Margarita Flores <mfloreszatarain95@gmail.com>
2022-06-17 14:25:13 -0600

    Se modifico

--- Saludo/script.js
+++ Saludo/script.js
@@ -2,16 +2,18 @@ console.log ("Se nota que todavia me extrañas");
 
 setInterval(cambiarSaludo, 2000);
 
+/**
+ * Cambia la frase en el titulo h2
+  */
+
 function cambiarSaludo() {
-  //console.log("Me parece una falta de respecto");
   let frase = document.getElementsByTagName("h2")[1].innerHTML;
 
-  if ( frase.innerHTML == "Ganando como siempre") {
-    //frase = "Impongo moda para las niñas!";
-    frase.innerHTML = "Impongo moda para las niñas!";
+  if ( document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre") {
+    document.getElementsByTagName("h2")[1].innerHTML = "Impongo moda para las niñas!";
   } else {
-    //frase = "Ganando como siempre";
-    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+    frase.innerHTML = "Ganando como siempre";
   }
-  console.log("frase :" + frase);
+  //console.log("frase :" + frase);
 }
+
