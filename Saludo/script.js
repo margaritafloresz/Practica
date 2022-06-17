a84df4a4cea4da3f4b015f6927ffa7430b805c0d
Margarita Flores <mfloreszatarain95@gmail.com>
2022-06-17 14:13:24 -0600

    Agregue

--- Saludo/script.js
+++ Saludo/script.js
@@ -1,1 +1,17 @@
 console.log ("Se nota que todavia me extrañas");
+
+setInterval(cambiarSaludo, 2000);
+
+function cambiarSaludo() {
+  //console.log("Me parece una falta de respecto");
+  let frase = document.getElementsByTagName("h2")[1].innerHTML;
+
+  if ( frase.innerHTML == "Ganando como siempre") {
+    //frase = "Impongo moda para las niñas!";
+    frase.innerHTML = "Impongo moda para las niñas!";
+  } else {
+    //frase = "Ganando como siempre";
+    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre";
+  }
+  console.log("frase :" + frase);
+}
